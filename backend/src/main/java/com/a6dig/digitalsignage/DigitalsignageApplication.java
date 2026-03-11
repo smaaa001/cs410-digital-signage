8df9a1f7bbb55cad4ea0bc32083bcc1e11995ca7
smaaa001 <s.ahsanahmedabid001@umb.edu>
2026-03-11 06:30:28 -0400

    minimum project setup - backend

---
+++ backend/src/main/java/com/a6dig/digitalsignage/DigitalsignageApplication.java
@@ -0,0 +1,13 @@
+package com.a6dig.digitalsignage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalsignageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalsignageApplication.class, args);
+	}
+
+}
