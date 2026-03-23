63fa6820b4df3dea9b15aad8efdd1a2f6e21b0a0
CompSciOmanjuu <nhuvo2004@gmail.com>
2026-03-23 16:09:59 -0400

    Setup

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -1,16 +1,9 @@
 import './App.css'
 
-function MyButton() {
-  return (
-    <button>I'm a button</button>
-  );
-}
-
 function App() {
     return (
         <div>
             <h1>Digital Signage</h1>
-            <MyButton />
         </div>
     );
 }
