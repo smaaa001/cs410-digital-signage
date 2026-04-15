6b55c0badef8b105dbb54de9f79bb52ef534890d
CompSciOmanjuu <nhuvo2004@gmail.com>
2026-04-15 16:31:03 -0400

    Cleaned up Devices.css styling and implemented buttons for Devices.jsx

--- frontend/src/pages/Devices.jsx
+++ frontend/src/pages/Devices.jsx
@@ -23,10 +23,14 @@ function Devices() {
     )
 }
 
+// Device Groups Section
 function DeviceGroups() {
     return(
         <div className="subpage">
-            <h1>Device Groups</h1>
+            <div className="section-header">
+                <h1>Device Groups</h1>
+                <button>New Group</button>
+            </div>
             
             <GroupCards />
         </div>
@@ -36,16 +40,26 @@ function DeviceGroups() {
 function GroupCards() {
     return(
         <div className="groupcard">
-            <h3>Campus Center</h3>
+            <div className="section-header">
+                <h3>Campus Center</h3>
+
+                <div className="card-UI">
+                    <button>Edit</button>
+                    <button>Delete</button>
+                </div>
+            </div>
+            
+            {/* Card Information */}
             <p>2 devices | Layout: Campus Center Default</p>
         </div>
     )
 }
 
+// Devices Section
 function DeviceList(){
     return (
         <div className="subpage">
-            <div className="thing">
+            <div className="section-header">
                 <h1>Devices</h1>
                 <button>Add Device</button>
             </div>
@@ -58,11 +72,28 @@ function DeviceList(){
 
 function DeviceCards() {
     return(
-        <div className="devicecard">
-            <h3>Devices</h3>
+        <div className="card">
+            <div className="card-header">
+                <h3>CC Main Lobby</h3>
 
+                <div className="card-UI">
+                    <p>Online</p>
+
+                    <div>
+                        <button>View</button>
+                        <button>Edit</button>
+                        <button>Delete</button>
+                    </div>
+                </div>
+            </div>
+
+            {/* Card information */}
+            <p>Campus Center - Main Lobby</p>
+            <p>Group: Campus Center</p>
+            <p>Assigned Layout: Campus Center Default</p>
             
         </div>
+        
     )
 }
 
