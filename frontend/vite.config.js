8f820855ed441fb0aaee75c62d248c3272a24dfd
CompSciOmanjuu <nhuvo2004@gmail.com>
2026-05-03 15:29:04 -0400

    Changed App.jsx to route url's correctly for hdeblois's website, adjusted vite.config.js to match

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/~smaaa001/frontend/',
+  base: '/~hdeblois/cs410/longproj01/t6-DS/',
   plugins: [react()],
   server: {
     proxy: {
